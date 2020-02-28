9a2ad0a4793ea87a068e05b56cd54786d2a0dfc8
Dev <basvanpol@Bass-MacBook-Pro.local>
2020-02-28 23:53:19 +1300

    fixing feed post collector

--- server/src/services/feedPostCollector.ts
+++ server/src/services/feedPostCollector.ts
@@ -40,7 +40,7 @@ const getFeedPosts = async () => {
     if (tokens && tokens.length > 0) {
         Subscription.find({ memberCount: { $gt: 0 } }, async (err, subscriptions) => {
             if (err) {
-                
+                console.log('something went wrong when searching subscription');
             }
             subQueue = subscriptions;
             let tokenIndex = 0;
@@ -63,7 +63,7 @@ const getFeedPosts = async () => {
                         const since_id = (currentSubscription.sinceId) ? currentSubscription.sinceId : '';
                         const max_id = (currentSubscription.maxId) ? currentSubscription.maxId : '';
                         const feed_id = (currentSubscription.feed.feedId) ? currentSubscription.feed.feedId : '';
-                        // 
+                        // console.log('currentSubscription.feed', currentSubscription.feed);
                         if (feed_id) {
                             if (since_id) {
                                 await oauth.get(
@@ -71,24 +71,23 @@ const getFeedPosts = async () => {
                                     token.token, //test user token
                                     token.tokenSecret, //test user secret            
                                     (err, data, result) => {
-                                        // 
-                                        if (err) 
+                                        // console.log('with since id', feed_id);
+                                        if (err) console.error(err);
                                         parseTwitterPostsData(currentSubscription, feed_id, data);
                                     });
-                                    
+
                             } else {
                                 await oauth.get(
                                     `https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=${feed_id}&count=10}`,
                                     '378751182-osBanchlb3uXld55fvplBW6weEChmQBbOrhmPb2r', //test user token
                                     'QxPyt7u4cx25kH0KHnZWCfbk2kxceYALhtyAasw4kNk', //test user secret            
                                     (err, data, result) => {
-                                        // 
-                                        if (err) 
-                                        
+                                        // console.log('without since id', feed_id);
+                                        if (err) console.error(err);
                                         parseTwitterPostsData(currentSubscription, feed_id, data);
                                     });
                             }
-                            
+
                         }
                     } else {
                         // find another way of getting a token
@@ -103,51 +102,51 @@ const getFeedPosts = async () => {
 
     const parseTwitterPostsData = async (subscription, feedId, data) => {
         let oData = JSON.parse(data); //reverse since the oldest tweets need to be saved first, otherwise next time new tweets will be saved in wrong order after previously saved tweets
-        if(oData.length > 0){
+        if (oData.length > 0) {
             oData = [...oData].reverse();
         } else {
             return;
         }
         let i = 0;
         let newestPostIndex = oData.length - 1;
-        
+        console.log('data length ', oData.length);
         for (let key in oData) {
-            // 
-            if(oData[key].id_str !== subscription.sinceId){
-                // 
-    
+            // console.log(`if ${oData[key].id_str} !== ${subscription.sinceId}`)
+            if (oData[key].id_str !== subscription.sinceId) {
+                // console.log('oData[key]', oData[key]);
+
                 let aContent = [];
                 if (oData[key].text) {
                     const oText = { "mainType": "TEXT", "type": "TEXT_TWITTER", "source": oData[key].text, "date": null, "location": null, "thumb": null }
                     aContent.push(oText);
                 }
-                    
+                console.log('oData[key]', oData[key]);
                 const entities = oData[key].entities;
-                
+
                 if (entities.urls) {
-                    
+                    console.log('urls: ', entities.urls);
                     const urls = entities.urls;
                     const firstUrl = entities.urls[0];
                     if (firstUrl && firstUrl.expanded_url) {
-                        const oLink = { "mainType": "LINK" , "type": "LINK_TWITTER", "source": entities.urls[0].expanded_url, "date": null, "location": null, "thumb": null }
+                        const oLink = { "mainType": "LINK", "type": "LINK_TWITTER", "source": entities.urls[0].expanded_url, "date": null, "location": null, "thumb": null }
                         aContent.push(oLink);
                     }
                 }
-    
+
                 if (entities.media) {
                     const oMedia = { "mainType": "IMAGE", "type": "IMAGE_TWITTER", "source": entities.media[0].media_url, "date": null, "location": null, "thumb": null };
                     aContent.push(oMedia);
                 }
-    
+
                 const oUser = oData[key].user;
                 let metaData = {
                     "authorname": "@" + oUser.screen_name,
                     "authorThumb": oUser.profile_image_url,
                     "name": oUser.name
                 };
-    
+
                 const newPost = await new Post();
-    
+
                 newPost.feedId = feedId;
                 newPost.date = new Date(oData[key].created_at);
                 newPost.contents = aContent;
@@ -155,12 +154,12 @@ const getFeedPosts = async () => {
                 // save feed post
                 await newPost.save((err) => {
                     if (err) {
-                        
-                        if (err) {  }
+                        console.log(' errrr ', err);
+                        if (err) { console.log(err) }
                     }
-                })    
+                })
             } else {
-                
+                console.log(' hey maar, toch n dubbele!', oData[key].text);
             }
 
             if (i === newestPostIndex) {
@@ -168,21 +167,19 @@ const getFeedPosts = async () => {
                 if (subscription.sinceId && subscription.sinceId.toString() === sinceId.toString()) {
                     return;
                     // the data set is the same, don't do anything, since the sinceId is the most current
-                } else{
+                } else {
                     subscription.sinceId = sinceId;
                     await subscription.save((err) => {
-                        if (err) {  }
+                        if (err) { console.log(err) }
                     })
                 }
-                
-                
-                
-                
+                console.log('sinceId ', sinceId);
+                console.log(' the latest I presume: ', oData[key].urls);
             };
-            
+
             i++;
         }
-        
+
     }
 }
 
