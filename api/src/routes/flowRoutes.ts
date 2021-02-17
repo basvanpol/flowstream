c636137078374731e9b18910dc51733a56b38170
Dev <basvanpol@Bass-MacBook-Pro.local>
2021-02-17 13:52:37 +1300

    added es2017 lib to api tsconfig and fixed issue in flowRoutes

--- api/src/routes/flowRoutes.ts
+++ api/src/routes/flowRoutes.ts
@@ -27,7 +27,7 @@ export default (app) => {
                 //     // save flow
                 // } else {
                 
-                const newFlow: IFlow = await Flow() as IFlow;
+                const newFlow: IFlow = new Flow() as IFlow;
                 newFlow._id = new mongoose.Types.ObjectId();
                 newFlow._user = req.user._id;
                 newFlow.title = (req.body.title) ? req.body.title : req.body.flowTitle;
